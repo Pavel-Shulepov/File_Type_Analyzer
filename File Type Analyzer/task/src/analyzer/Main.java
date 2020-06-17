4fb0ac44f32395fbc4f516d26a8a533929c52cf5
Pavel Shulepov <pavel.shulepov@coderlink.cc>
2020-06-17 20:57:36 +0500

    stage 3

--- File Type Analyzer/task/src/analyzer/Main.java
+++ File Type Analyzer/task/src/analyzer/Main.java
@@ -2,36 +2,59 @@ package analyzer;
 
 
 import analyzer.strategy.KmpSearchStrategy;
-import analyzer.strategy.NaiveSearchStrategy;
 import analyzer.strategy.SearchSubstring;
 import analyzer.utils.FileUtil;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws InterruptedException {
 
-        if (args.length < 4) throw new RuntimeException("Введите параметры!");
-        String algorithm = args[0];
-        String filePath = args[1];
-        String pattern = args[2];
-        String fileType = args[3];
+        if (args.length < 3) throw new RuntimeException("Введите параметры!");
+        String folderPath = args[0];
+        String pattern = args[1];
+        String fileType = args[2];
 
-        SearchSubstring searchSubstring;
-        if ("--naive".equals(algorithm)) {
-            searchSubstring = new SearchSubstring(new NaiveSearchStrategy());
-        } else if ("--KMP".equals(algorithm)) {
-            searchSubstring = new SearchSubstring(new KmpSearchStrategy());
-        } else throw new RuntimeException("Неверная стратегия поиска!");
+        List<File> fileList = new Vector<>();
+        FileUtil.getFilesRecursive(new File[]{ new File(folderPath) }, fileList);
 
+        SearchSubstring searchSubstring = new SearchSubstring(new KmpSearchStrategy());
+        ExecutorService WORKER_THREAD_POOL = Executors.newFixedThreadPool(10);
 
-        long start = System.nanoTime();
-        if (searchSubstring.contains(FileUtil.readFile(filePath), pattern)) System.out.println(fileType);
-        else System.out.println("Unknown file type");
-        long elapsedNanos = System.nanoTime() - start;
-        System.out.println("It took " + elapsedNanos / 1000000000 + " seconds");
+        Callable<String> searcher = () -> {
+            while (fileList.size() > 0) {
+                var f = fileList.remove(0);
+                try {
+                    if (searchSubstring.contains(FileUtil.readFile(f.getPath()), pattern)) System.out.println(f.getName() + ": " + fileType);
+                    else System.out.println(f.getName() + ": " + "Unknown file type");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        };
+
+        List<Callable<String>> searches = new ArrayList<>();
+        for (int i = 0; i < 10; i++) searches.add(searcher);
+        WORKER_THREAD_POOL.invokeAll(searches);
+        awaitTerminationAfterShutdown(WORKER_THREAD_POOL);
+        WORKER_THREAD_POOL.shutdown();
 
     }
 
+    public static void awaitTerminationAfterShutdown(ExecutorService threadPool) {
+        threadPool.shutdown();
+        try {
+            if (!threadPool.awaitTermination(60, TimeUnit.SECONDS)) {
+                threadPool.shutdownNow();
+            }
+        } catch (InterruptedException ex) {
+            threadPool.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
 }
--- File Type Analyzer/task/src/analyzer/utils/FileUtil.java
+++ File Type Analyzer/task/src/analyzer/utils/FileUtil.java
@@ -1,13 +1,28 @@
 package analyzer.utils;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
 
 public class FileUtil {
 
     public static byte[] readFile(String filePath) throws IOException {
         return Files.readAllBytes(Paths.get(filePath));
+    }
+
+    public static void getFilesRecursive(File[] arr, List<File> fileList)
+    {
+        for (File f : arr)
+        {
+            if(f.isFile()) fileList.add(f);
+            else if(f.isDirectory())
+            {
+                getFilesRecursive(Objects.requireNonNull(f.listFiles()), fileList);
+            }
+        }
     }
 
 }
